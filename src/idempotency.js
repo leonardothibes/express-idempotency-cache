a3c1baf9d7fe140f18cdff87d399c8f945f9766e
Leonardo Thibes <leonardothibes@gmail.com>
2023-04-26 23:55:03 -0300

    wip

--- src/idempotency.js
+++ src/idempotency.js
@@ -2,15 +2,16 @@
 
 class Idempotency
 {
-    get defaultConfig() { return { adapter: 'memory' } }
+    get defaultConfig() { return { adapter: 'memory', ttl: 86400 } }
 
     /**
      * @param {Object} config
      */
     constructor(config)
     {
-        this.config  = config || this.defaultConfig
-        this.adapter = this._getAdapter()
+        this.config      = config || this.defaultConfig
+        this.adapterName = String(this.config.adapter).trim().toLowerCase()
+        this.adapter     = this._getAdapter()
     }
 
     /**
@@ -28,7 +29,7 @@ class Idempotency
     /**
      * Calculate idempotency id from request.
      */
-    getKey(request)
+    key(request)
     {
         return 'caculatedKeyFromRequest'
     }
@@ -36,12 +37,10 @@ class Idempotency
     _getAdapter()
     {
         try {
-            const name   = String(this.config.adapter).trim().toLowerCase()
-            const driver = require(`./adapter/${name}`)
-
+            const driver = require(`./adapter/${this.adapterName}`)
             return new driver(this.config)
         } catch (e) {
-            const message = `Cannot instantiate idempotency driver: ${adapterName}`
+            const message = `Cannot instantiate idempotency driver: ${this.adapterName}`
             throw Error(message)
         }
     }
