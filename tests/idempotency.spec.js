a46b1bdca63ef8f1675b1bc31c4176ee64949119
Leonardo Thibes <leonardothibes@gmail.com>
2023-08-25 15:38:03 -0300

    feat(content-type): adding compatibility with "x-www-form-urlencoded"

--- tests/idempotency.spec.js
+++ tests/idempotency.spec.js
@@ -4,19 +4,75 @@ const idempotency = require('../src/idempotency')
 
 describe('idempotency', () =>
 {
-    it('key(request)', (done) =>
+    const methods = ['POST', 'PUT', 'PATCH', 'DELETE']
+
+    describe('key(request)', () =>
     {
-        const config   = { adapter: 'mock' }
-        const instance = idempotency.getInstance(config)
+        methods.forEach(method =>
+        {
+            it(`key(request) [${method}]`, (done) =>
+            {
+                const config   = { adapter: 'mock' }
+                const instance = idempotency.getInstance(config)
 
-        const request = {
-            method: 'POST',
-            body  : { foo: 'bar' }
-        }
+                const body    = { foo: 'bar' }
+                const request = { method, body }
 
-        const key = instance.key(request)
-        expect(key).toEqual('86132018186ec5005e03303957837c95')
+                const key = instance.key(request)
+                expect(key).toEqual('86132018186ec5005e03303957837c95')
 
-        done()
+                done()
+            })
+        })
+    })
+
+    describe('decode(body)', () =>
+    {
+        methods.forEach(method =>
+        {
+            it(`decode(body) [${method}]`, (done) =>
+            {
+                const config   = { adapter: 'mock' }
+                const cache  = idempotency.getInstance(config)
+
+                const body    = { foo: 'bar' }
+                const request = { method, body }
+
+                const output = cache.decode(request)
+                expect(output).toEqual(JSON.stringify(request.body))
+
+                done()
+            })
+        })
+    })
+
+    describe('stringify(body)', () =>
+    {
+        methods.forEach(method =>
+        {
+            it(`stringify(body) [${method}]`, (done) =>
+            {
+                const config = { adapter: 'mock' }
+                const cache  = idempotency.getInstance(config)
+
+                const body   = { foo: 'bar' }
+                const output = cache.stringify(body)
+
+                expect(output).toEqual(JSON.stringify(body))
+                done()
+            })
+        })
+
+        it('stringify(body) [GET]', (done) =>
+        {
+            const config = { adapter: 'mock' }
+            const cache  = idempotency.getInstance(config)
+
+            const body   = undefined
+            const output = cache.stringify(body)
+
+            expect(output).toEqual('{}')
+            done()
+        })
     })
 })
